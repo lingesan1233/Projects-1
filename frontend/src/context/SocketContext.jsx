78abf4de87a9468f43e3c90b9e43ef968d84951e
lingesan <sglingesan@gmail.com>
2025-05-18 22:59:47 +0530

    f4

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatapp-pro1.onrender.com", {
+			const socket = io("https://chat-app-pro1-7y2z.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
