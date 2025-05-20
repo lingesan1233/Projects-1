8436741d91d38c6f0e6dfeb9fdc36928af3753d7
lingesan <sglingesan@gmail.com>
2025-05-20 15:27:08 +0530

    r1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: "dist",
+  }
 });
